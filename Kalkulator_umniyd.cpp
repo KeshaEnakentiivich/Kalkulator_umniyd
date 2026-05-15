e7428d8a60785ba2e6554a502372e9d5ed3950b1
KeshaEnakentiivich <fgdf456786458675765879765765rt@gmail.com>
2026-05-15 19:14:17 +0300

    Menu func

--- Kalkulator_umniyd.cpp
+++ Kalkulator_umniyd.cpp
@@ -1,14 +1,108 @@
 ﻿#include "mthfuncs.h"
 #include <iostream>
+#include <windows.h>
 using namespace std;
-int main()
+string menuopt;
+int o = 0;
+int d = 0;
+bool fd = false;
+int main(bool ff)
 {
-    cout << "2 + 2 =";
-    cout << sum(2, 2) << endl;
-    cout << "8 - 1 =";
-    cout << mns(8, 1) << endl;
-    cout << "93/28 =";
-    cout << divd(93, 28) << endl;
-    cout << "515151 x 676767 =";
-    cout << mul(515151, 676767) << endl;
+    if (!fd)
+    {
+        cout << "Vyberite optsiyu:" << endl;
+        cout << "1. Slozhenie\n" << endl;
+        cout << "2. Vychitanie\n" << endl;
+        cout << "3. Delenie\n" << endl;
+        cout << "4. Umnozhenie\n" << endl;
+        cout << "Vyberite: " << endl;
+        cin >> menuopt;
+        if (menuopt == "1")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "+" + d << endl;
+            cout << sum(o, d) << endl;
+        }
+        else if (menuopt == "2")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "-" + d << endl;
+            cout << mns(o, d) << endl;
+        }
+        else if (menuopt == "3")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + ":" + d << endl;
+            cout << divd(o, d) << endl;
+        }
+        else if (menuopt == "4")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "x" + d << endl;
+            cout << mul(o, d) << endl;
+        }
+        else
+        {
+            cout << "Vyberite optsiyu esche raz!" << endl;
+        }
+        fd = true;
+        return main(true);
+    }
+    else
+    {
+        cout << "Vyberite: " << endl;
+        cin >> menuopt;
+        if (menuopt == "1")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "+" + d << endl;
+            cout << sum(o, d) << endl;
+        }
+        else if (menuopt == "2")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "-" + d << endl;
+            cout << mns(o, d) << endl;
+        }
+        else if (menuopt == "3")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + ":" + d << endl;
+            cout << divd(o, d) << endl;
+        }
+        else if (menuopt == "4")
+        {
+            cout << "Pervoe chislo: " << endl;
+            cin >> o;
+            cout << "Vtoroe chislo: " << endl;
+            cin >> d;
+            cout << o + "x" + d << endl;
+            cout << mul(o, d) << endl;
+        }
+        else
+        {
+            cout << "Vyberite optsiyu esche raz!" << endl;
+        }
+    }
 }
